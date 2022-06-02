53b9babad2de66f5486c446fce6fb4d469e7f77d
“isaacwambu” <codeacademyj@gmail.com>
2022-06-02 13:44:02 +0300

    add the toggle effect on vision, mission and objectives divs using js

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#vision').on('click', function(){
+        $('#vis').toggle();
+
+    });
+    $('#mission').on('click', function(){
+        $('#mis').toggle();
+
+    });
+    $('#objectives').on('click', function(){
+        $('#obj').toggle();
+
+    });
+});
